382e9f1bb63ddc1a5e94302e16d82beb234dcf54
Jeff Carpenter <gcarpenterv@gmail.com>
2014-06-05 13:48:34 -0700

    Update URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject metar "0.1.0"
   :description "METAR weather report parser for Clojure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/jeffcarp/metar"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
