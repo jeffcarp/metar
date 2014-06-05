112df6de9f8a3c6ba12f993adcef5cc47bd15246
Jeff Carpenter <gcarpenterv@gmail.com>
2014-06-04 18:07:24 -0700

    Begin adding naive implementation
    This is not based off of any METAR spec. Before this is production ready I'll have to go back over it with the METAR spec in hand and add a bunch more test cases to make sure it conforms to all kinds of METARS.

--- src/metar/core.clj
+++ src/metar/core.clj
@@ -1,6 +1,22 @@
-(ns metar.core)
+(ns metar.core
+  (:require [clojure.string :as string :only [split]]))
 
 (defn parse
-  "I don't do a whole lot."
-  []
-  "METAR")
+  "Takes a string, retuns METAR weather data."
+  [metar-str]
+  (let [parts (string/split metar-str #" ")
+        airport (first parts)
+        datetime (nth parts 1)
+        day (subs datetime 0 2)
+        wind (nth parts 2)
+        direction (subs wind 0 3)
+        speed (subs wind 3)
+        wind-speed (last (re-find #"(\d+)[G|KT]" speed)) 
+        wind-gust (last (re-find #"G(\d+)KT" speed))]
+    {
+      :airport airport 
+      :day day 
+      :wind-direction direction
+      :wind-speed-knots wind-speed
+      :wind-gust-knots wind-gust
+    }))
--- test/metar/core_test.clj
+++ test/metar/core_test.clj
@@ -2,10 +2,11 @@
   (:require [clojure.test :refer :all]
             [metar.core :refer :all]))
 
-(def sample-data [
-	"KSFO 042256Z 30015G22KT 10SM FEW008 19/11 A2985 RMK AO2 SLP107 T01940111"
- ])
-
 (deftest test-parse
-  (testing "parses date"
-    (is (= (metar.core/parse "KSFO 042256Z 30015G22KT 10SM FEW008 19/11 A2985 RMK AO2 SLP107 T01940111") "METAR"))))
+  (testing "parses airport"
+    (let [result (metar.core/parse "KSFO 042256Z 30015G22KT 10SM FEW008 19/11 A2985 RMK AO2 SLP107 T01940111")]
+    (is (map? result))
+    (is (= (:airport result) "KSFO"))
+    (is (= (:wind-direction result) "300"))
+    (is (= (:wind-speed-knots result) "15"))
+    (is (= (:wind-gust-knots result) "22")))))
