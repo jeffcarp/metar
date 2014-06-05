0c487bc1ab2e2a8333fb35c873f327be209b35b9
Jeff Carpenter <gcarpenterv@gmail.com>
2014-06-05 14:46:37 -0700

    Refactor parsing into individual functions
    I'm not sure whether to make these functions able to handle the whole METAR, or only the string section that they're dealing with. It would probably be easiest on the user and not too difficult to allow them to accept both.

--- src/metar/core.clj
+++ src/metar/core.clj
@@ -5,27 +5,44 @@
   "Validates a METAR."
   [metar-str]
   (let [parts (string/split metar-str #" ")]
-    (println "count parts" (count parts) parts)
-    (boolean
-      (and
-        (< 4 (count parts))))))
+    (and
+      (< 4 (count parts)))))
+
+(defn part
+  "Returns the requested string part of the METAR."
+  [metar-str ident]
+  (let [parts (string/split metar-str #" ")]
+    (case ident
+      "airport"  (nth parts 0)
+      "datetime" (nth parts 1)
+      "wind"     (nth parts 2))))
+
+(defn day
+  "Extracts the day of the month from the datetime."
+  [datetime-str]
+  (subs datetime-str 0 2))
+
+(defn wind-direction
+  [wind-str]
+  (subs wind-str 0 3))
+
+(defn wind-speed 
+  [wind-str]
+  (last (re-find #"(\d+)[G|KT]" (subs wind-str 3))))
+
+(defn wind-gust
+  [wind-str]
+  (last (re-find #"G(\d+)KT" (subs wind-str 3))))
 
 (defn parse
   "Takes a string, retuns METAR weather data."
   [metar-str]
-  (let [parts (string/split metar-str #" ")
-        airport (first parts)
-        datetime (nth parts 1)
-        day (subs datetime 0 2)
-        wind (nth parts 2)
-        direction (subs wind 0 3)
-        speed (subs wind 3)
-        wind-speed (last (re-find #"(\d+)[G|KT]" speed)) 
-        wind-gust (last (re-find #"G(\d+)KT" speed))]
+  (let [section (partial part metar-str)]
     {
-      :airport airport 
-      :day day 
-      :wind-direction direction
-      :wind-speed-knots wind-speed
-      :wind-gust-knots wind-gust
+      :airport (section "airport") 
+      :day (day (section "datetime"))
+      :wind-direction (wind-direction (section "wind"))
+      :wind-speed-knots (wind-speed (section "wind"))
+      :wind-gust-knots (wind-gust (section "wind"))
     }))
+
--- test/metar/core_test.clj
+++ test/metar/core_test.clj
@@ -4,17 +4,34 @@
             [metar.core :as core]))
 
 (deftest valid
-  (testing "parses airport"
+  (testing "returns true for a valid METAR"
     (let [result (core/valid (first data/metars))]
-    (is (= true result))
-)))
+      (is (true? result))))
+
+  (testing "returns false for an invalid METAR"
+    (let [result (core/valid "stuff")]
+      (is (false? result)))))
 
 (deftest parse
   (testing "parses airport"
     (let [result (core/parse (first data/metars))]
     (is (map? result))
     (is (= (:airport result) "KSFO"))
+    (is (= (:day result) "04"))
     (is (= (:wind-direction result) "300"))
     (is (= (:wind-speed-knots result) "15"))
     (is (= (:wind-gust-knots result) "22")))))
+
+(deftest part
+  (let [section (partial part (first data/metars))]
+    (testing "parses airport"
+      (is (= "KSFO" (section "airport"))))
+    (testing "parses datetime"
+      (is (= "042256Z" (section "datetime"))))
+    (testing "parses wind"
+      (is (= "30015G22KT" (section "wind"))))))
+
+(deftest day
+  (testing "handles regular input"
+    (is (= "04" (core/day "042256Z")))))
 
