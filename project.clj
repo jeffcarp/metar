678889b59de67185e649fa95b8d2ae61df7cdb72
Jeff Carpenter <gcarpenterv@gmail.com>
2014-06-06 02:21:56 -0700

    Bump version to 0.3.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metar "0.2.0"
+(defproject metar "0.3.0-SNAPSHOT"
   :description "METAR weather report parser for Clojure"
   :url "https://github.com/jeffcarp/metar"
   :license {:name "Eclipse Public License"
